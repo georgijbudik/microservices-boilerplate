b6e152744016d1c37933aa9701cd0cb2f199a257
georgijbudik <georgijbudik@gmail.com>
2024-02-25 21:56:23 +0100

    post service

---
+++ posts/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+    status: "pending",
+  };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+      status: "pending",
+    },
+  });
+
+  res.status(201).json(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  const { type } = req.body;
+
+  console.log("Received event", type);
+
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
